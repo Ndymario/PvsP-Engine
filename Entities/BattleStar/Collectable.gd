d97ceef234a771ca7645cad992d1be2bd580abe9
Nolan Y? <ndymario@gmail.com>
2023-05-24 23:16:08 -0500

    Add idle and collected animations for Big Stars

--- Entities/BattleStar/Collectable.gd
+++ Entities/BattleStar/Collectable.gd
@@ -3,7 +3,16 @@ extends Node
 class_name Collectable
 
 @export var collected: bool = false
+@export var animation_tree: AnimationTree
 
 func collect():
-	get_parent().queue_free()
-	collected = true
+	if animation_tree != null:
+		animation_tree.get("parameters/playback").travel("collected")
+	else:
+		get_parent().queue_free()
+		collected = true
+
+func _on_animation_tree_animation_finished(anim_name):
+	if anim_name == "collected":
+		get_parent().queue_free()
+		collected = true
